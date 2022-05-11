8195bd7534073a26ea941bc4d7d5a9bb50991423
Dominik <dominik.merdzik@onet.pl>
2022-05-11 20:54:42 +0200

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
-import UserFormHolder from './components/UserForm/UserFormHolder';
+import React from 'react';
 import './App.css';
 
-const INITIAL_USER_DATA = [
-  {
-    userName: 'Max',
-    userAge: '31'
-  }
-];
 
 const App = () => {
 
-  const [usersArray, setUsersArray] = useState(INITIAL_USER_DATA)
-
-  const userAdd = (userObject) => {
-    setUsersArray((prevUsers => {
-      return [userObject, ...prevUsers];
-    }));
-  }
-
   return (
     <div>
-      <UserFormHolder 
-        className="userFormHolder" 
-        onReceiveUsersData={userAdd}
-        users={usersArray}
-      />
+      
     </div>
   );
 }
